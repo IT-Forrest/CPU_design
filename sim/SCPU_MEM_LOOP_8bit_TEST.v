126b6270671a3388c10b63ca1e4f63606fed423f
wohenniubi <jiafan0420@tamu.edu>
2016-03-12 19:46:13 -0600

    Pass the memory loop test; Good !!
    (1) the memory loop test is passed where only one 512x8bit SRAM is used;
    32x8bit is for Data, others for instructions;
    (2) SRAM is not generated by Memory_compiler;

--- sim/SCPU_MEM_LOOP_8bit_TEST.v
+++ sim/SCPU_MEM_LOOP_8bit_TEST.v
@@ -7,7 +7,7 @@
 // VERSION      : 1.0
 // PURPOSE      : the SCPU test with self-created SRAM
 // --------------------------------------------------------------------
-// ABSTRACT: simulation time 13us given each time period 10ns
+// ABSTRACT: ModelSim simulation time 6us given each time period 10ns
 // --------------------------------------------------------------------
 `timescale 1ns / 1ps
 `include "../DEFINE_CPU.v"
@@ -25,9 +25,10 @@ module SCPU_MEM_LOOP_8BIT_TOP;
     
     // Wires
     wire is_i_addr;
-    wire [7:0] i_datain;
-    wire [7:0] d_datain;
-    wire [7:0] d_dataout;
+    wire [7:0]  i_datain;
+    wire [7:0]  d_datain;
+    wire [7:0]  d_dataout;
+    wire [7:0]  m_datain;
 
     wire [7:0]  m_dataout;
     wire [8:0]  m_addr;
@@ -35,7 +36,8 @@ module SCPU_MEM_LOOP_8BIT_TOP;
     wire [8:0]  d_addr;
     wire d_we;
 
-    integer i;
+    integer i,j;
+    reg  [15:0] tmpi_datain;
     
     // Instantiate the Unit Under Test (UUT)
     SERIAL_CPU_8BIT uut (
@@ -61,10 +63,13 @@ module SCPU_MEM_LOOP_8BIT_TOP;
         .dataout(m_dataout)
     );
 
+    parameter   DEFAULT_PC_ADDR = 16;
+    defparam    uut.DEFAULT_PC_ADDR = DEFAULT_PC_ADDR;
+    
     assign  m_addr = (is_i_addr)?i_addr:d_addr;
     assign  m_datain = d_dataout;
-    assign  i_datain = m_dataout;
-    assign  d_datain = m_dataout;
+    assign  i_datain = (is_i_addr)?m_dataout:0;
+    assign  d_datain = (is_i_addr)?0:m_dataout;
     
     initial begin
         // Initialize Inputs
@@ -85,40 +90,69 @@ module SCPU_MEM_LOOP_8BIT_TOP;
             d_addr, d_dataout, d_we, uut.gr[1], uut.gr[2], uut.gr[3],
             uut.zf, uut.nf, uut.cf);
 
-        i_mem.I_RAM[ 0] = {`SUB, `gr7, 1'b0, `gr7, 1'b0, `gr7};//reset the loop controller `gr7
-        i_mem.I_RAM[ 1] = {`SUB, `gr1, 1'b0, `gr1, 1'b0, `gr1};//reset the sum value
-        i_mem.I_RAM[ 2] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[ 3] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[ 4] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[ 5] = {`ADDI, `gr7, 4'b0001, 4'b1001};//set the loop controller `gr7 = 25
-        i_mem.I_RAM[ 6] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[ 7] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[ 8] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[ 9] = {`ADD, `gr1, 1'b0, `gr1, 1'b0, `gr7};//sum += `gr7
-        i_mem.I_RAM[10] = {`SUBI, `gr7, 4'b0000, 4'b0001};
-        i_mem.I_RAM[11] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[12] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[13] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[14] = {`BNZ, `gr0, 4'b0000, 4'b1001};//if (`gr7 != 0) go to I_RAM[ 9];
-        i_mem.I_RAM[15] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[16] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[17] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[18] = {`STORE, `gr1, 1'b0, `gr0, 4'b0010};
-        i_mem.I_RAM[19] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[20] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[21] = {`NOP, 11'b000_0000_0000};
-        i_mem.I_RAM[22] = {`HALT, 11'b000_0000_0000};//due to the pipeline, we need to add many `NOP to the instruction set
+        i= DEFAULT_PC_ADDR*2;
+        tmpi_datain = {`SET, `gr3, 4'b0000, 4'b0100};//reset the loop controller `gr7
+        i_mem.I_RAM[ i] = tmpi_datain[7:0];  i = 1 + DEFAULT_PC_ADDR*2;
+        i_mem.I_RAM[ i] = tmpi_datain[15:8]; i = 2 + DEFAULT_PC_ADDR*2;
+        tmpi_datain = {`SET, `gr1, 4'b0000, 4'b0000};//reset the sum value
+        i_mem.I_RAM[ i] = tmpi_datain[7:0];  i = 3 + DEFAULT_PC_ADDR*2;
+        i_mem.I_RAM[ i] = tmpi_datain[15:8]; i = 4 + DEFAULT_PC_ADDR*2;
+        // i_mem.I_RAM[ 2] = {`NOP, 11'b000_0000_0000};
+        // i_mem.I_RAM[ 3] = {`NOP, 11'b000_0000_0000};
+        // i_mem.I_RAM[ 4] = {`NOP, 11'b000_0000_0000};
+        tmpi_datain = {`ADD, `gr1, 1'b0, `gr1, 1'b0, `gr3};//set the loop controller `gr7 = 25
+        i_mem.I_RAM[ i] = tmpi_datain[7:0];  i = 5 + DEFAULT_PC_ADDR*2;
+        i_mem.I_RAM[ i] = tmpi_datain[15:8]; i = 6 + DEFAULT_PC_ADDR*2;
+        // i_mem.I_RAM[ 7] = {`NOP, 11'b000_0000_0000};
+        // i_mem.I_RAM[ 8] = {`NOP, 11'b000_0000_0000};
+        tmpi_datain = {`SUBI, `gr3, 4'b0000, 4'b0001};//sum += `gr7
+        i_mem.I_RAM[ i] = tmpi_datain[7:0];  i = 7 + DEFAULT_PC_ADDR*2;
+        i_mem.I_RAM[ i] = tmpi_datain[15:8]; i = 8 + DEFAULT_PC_ADDR*2;
         
-        d_mem.D_RAM[0] = 16'h00AB;
-        d_mem.D_RAM[1] = 16'h3C00;
-        d_mem.D_RAM[2] = 16'h0000;
+        //if (`gr3 != 0) go to I_RAM[ 9];
+        //make sure to include the offset for DATA SRAM
+        tmpi_datain = {`BNZ, `gr0, 4'b0001, 4'b0010};
+        
+        i_mem.I_RAM[ i] = tmpi_datain[7:0];  i = 9 + DEFAULT_PC_ADDR*2;
+        i_mem.I_RAM[ i] = tmpi_datain[15:8]; i = 10+ DEFAULT_PC_ADDR*2;
+        // i_mem.I_RAM[11] = {`NOP, 11'b000_0000_0000};
+        // i_mem.I_RAM[12] = {`NOP, 11'b000_0000_0000};
+        // i_mem.I_RAM[13] = {`NOP, 11'b000_0000_0000};
+        tmpi_datain = {`STORE, `gr1, 1'b0, `gr0, 4'b0010};//if (`gr7 != 0) go to I_RAM[ 9];
+        i_mem.I_RAM[ i] = tmpi_datain[7:0];  i = 11+ DEFAULT_PC_ADDR*2;
+        i_mem.I_RAM[ i] = tmpi_datain[15:8]; i = 12+ DEFAULT_PC_ADDR*2;
+        // i_mem.I_RAM[15] = {`NOP, 11'b000_0000_0000};
+        // i_mem.I_RAM[16] = {`NOP, 11'b000_0000_0000};
+        // i_mem.I_RAM[17] = {`NOP, 11'b000_0000_0000};
+        tmpi_datain = {`HALT, 11'b000_0000_0000};//due to the pipeline, we need to add many `NOP to the instruction set
+        i_mem.I_RAM[ i] = tmpi_datain[7:0];  i = 13+ DEFAULT_PC_ADDR*2;
+        i_mem.I_RAM[ i] = tmpi_datain[15:8]; i = 14+ DEFAULT_PC_ADDR*2;
+        // i_mem.I_RAM[19] = {`NOP, 11'b000_0000_0000};
+        // i_mem.I_RAM[20] = {`NOP, 11'b000_0000_0000};
+        // i_mem.I_RAM[21] = {`NOP, 11'b000_0000_0000};
+        
+        i = 0;
+        tmpi_datain = 16'h00AB;
+        i_mem.I_RAM[ i] = tmpi_datain[7:0];  i = 1;
+        i_mem.I_RAM[ i] = tmpi_datain[15:8]; i = 2;
+        tmpi_datain = 16'h3C00;
+        i_mem.I_RAM[ i] = tmpi_datain[7:0];  i = 3;
+        i_mem.I_RAM[ i] = tmpi_datain[15:8]; i = 4;
+        tmpi_datain = 16'h0000;
+        i_mem.I_RAM[ i] = tmpi_datain[7:0];  i = 5;
+        i_mem.I_RAM[ i] = tmpi_datain[15:8]; i = 6;
+        // i_mem.D_RAM[0] = 16'h00AB;
+        // i_mem.D_RAM[1] = 16'h3C00;
+        // i_mem.D_RAM[2] = 16'h0000;
 
         #10 rst_n = 0;
         #10 rst_n = 1;
         #10 enable = 1;
         #10 start =1;
         #10 start = 0;
-        #3130 $stop();
+        // for (j=1; j<40; j=j+1)
+            // #100;
+        #3130 $stop();//
     end
     
     always #5
