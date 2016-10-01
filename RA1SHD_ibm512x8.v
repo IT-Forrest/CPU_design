3c51fd9d53587176b39821a749ebeac4d71722c9
wohenniubi <jiafan0420@tamu.edu>
2016-10-01 18:55:51 -0500

    generate SRAM with new tools and finish simulation
    (1) genertae SRAM verilog file with new Memory Compiler; and finish all
    the simulation tests

--- RA1SHD_ibm512x8.v
+++ RA1SHD_ibm512x8.v
@@ -11,11 +11,11 @@
 //      The copyright notice(s) in this Software does not indicate actual or
 //      intended publication of this Software.
 //      
-//      name:			High Speed/Density Single-Port SRAM Generator
-//           			IBM CMRF8SF-RVT Process
+//      name:			High Speed/Density Single Port SRAM Generator
+//           			IBM CMRF8SF-LPVT Process
 //      version:		2009Q1V1
-//      comment:		
-//      configuration:	 -instname "RA1SHD_IBM512X8" -words 512 -bits 8 -frequency 100 -ring_width 3.0 -mux 8 -write_mask off -wp_size 8 -top_layer met4 -power_type rings -horiz met2 -vert met3 -cust_comment "" -bus_notation on -left_bus_delim "[" -right_bus_delim "]" -pwr_gnd_rename "VDD:VDD,GND:VSS" -prefix "" -pin_space 0.0 -name_case upper -check_instname on -diodes on -inside_ring_type GND -drive 6 -asvm on -corners ff_1p32v_m40c,ff_1p65v_125c,tt_1p2v_25c,ss_1p08v_125c
+//      comment:		RA1SHD_IBM512X8
+//      configuration:	 -instname "RA1SHD_IBM512X8" -words 512 -bits 8 -frequency 100 -ring_width 3.0 -mux 8 -write_mask off -wp_size 8 -top_layer met4 -power_type rings -horiz met2 -vert met3 -cust_comment "RA1SHD_IBM512X8" -bus_notation on -left_bus_delim "[" -right_bus_delim "]" -pwr_gnd_rename "VDD:VDD,GND:VSS" -prefix "" -pin_space 0.0 -name_case upper -check_instname on -diodes on -inside_ring_type GND -drive 6 -asvm on -corners ff_1p32v_m40c,ff_1p65v_125c,tt_1p2v_25c,ss_1p08v_125c
 //
 //      Verilog model for Synchronous Single-Port Ram
 //
@@ -31,7 +31,7 @@
 //      Redundant Columns:          0
 //      Test Muxes                  Off
 //
-//      Creation Date:  2016-04-15 20:37:02Z
+//      Creation Date:  2016-09-26 19:58:05Z
 //      Version: 	2009Q1V1
 //
 //      Modeling Assumptions: This model supports full gate level simulation
